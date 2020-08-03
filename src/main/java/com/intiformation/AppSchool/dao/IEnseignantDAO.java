a33cfad55aa0edbed562ba6153582ca2214a6918
anais leseur <anais.leseur@gmail.com>
2020-08-03 13:29:29 +0200

    Enseignant DAO Impl et Interface

---
+++ src/main/java/com/intiformation/AppSchool/dao/IEnseignantDAO.java
@@ -0,0 +1,16 @@
+package com.intiformation.AppSchool.dao;
+
+import com.intiformation.AppSchool.modele.Enseignant;
+
+/**
+ * Interface DAO spécifique à un enseignant
+ * Hérite de IUniverselDAO
+ * 
+ * @author anais
+ *
+ */
+public interface IEnseignantDAO extends IUniverselDAO<Enseignant> {
+	
+	/*__________ Méthodés spécifiques à un enseignant __________*/ 
+
+}// end interface
