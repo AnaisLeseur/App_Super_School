aeea6a8923a1a76e2c1e36b0b2b7eb7b7d5faef4
anais leseur <anais.leseur@gmail.com>
2020-08-03 12:33:55 +0200

    DAO Admin

---
+++ src/main/java/com/intiformation/AppSchool/dao/AdministrateurDAOImpl.java
@@ -0,0 +1,48 @@
+package com.intiformation.AppSchool.dao;
+
+import java.util.List;
+
+import com.intiformation.AppSchool.modele.Administrateur;
+
+/**
+ * Implémentation concrète de la couche DAO pour un administrateur 
+ * Implémente l'interface IAdministrateurDAO
+ * 
+ * @author anais
+ *
+ */
+public class AdministrateurDAOImpl implements IAdministrateurDAO {
+
+	
+	
+	@Override
+	public void add(Administrateur t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Administrateur t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Administrateur> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Administrateur getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/intiformation/AppSchool/dao/IAdministrateurDAO.java
@@ -0,0 +1,17 @@
+package com.intiformation.AppSchool.dao;
+
+import com.intiformation.AppSchool.modele.Administrateur;
+
+/**
+ * Interface DAO spécifique à un administrateur
+ * Hérite de IUniverselDAO
+ * 
+ * @author anais
+ *
+ */
+public interface IAdministrateurDAO extends IUniverselDAO<Administrateur> {
+	
+	/*__________ Méthodés spécifiques à l'adminitrateur __________*/ 
+	
+
+}// end interface
