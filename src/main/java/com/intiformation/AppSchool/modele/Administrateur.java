7b9f0474a272a9a8dc9bf9bca06d3b7b484221c9
anais leseur <anais.leseur@gmail.com>
2020-08-03 10:45:17 +0200

    modif Administrateur

--- src/main/java/com/intiformation/AppSchool/modele/Administrateur.java
+++ src/main/java/com/intiformation/AppSchool/modele/Administrateur.java
@@ -1,5 +1,12 @@
 package com.intiformation.AppSchool.modele;
 
+/**
+ * classe modele pour un administrateur. 
+ * classe qui etend Personne
+ * 
+ * @author vincent
+ *
+ */
 public class Administrateur {
 
-}
+}// end Administrateur
