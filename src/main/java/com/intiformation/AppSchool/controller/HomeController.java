7636e8066b9f6b4e8a4ed211d8c5ae622474cc62
kevin wanaverbecq <kevin-wanaverbecq@hotmail.com>
2020-08-14 09:41:51 +0200

    intertionalization

--- src/main/java/com/intiformation/AppSchool/controller/HomeController.java
+++ src/main/java/com/intiformation/AppSchool/controller/HomeController.java
@@ -1,29 +1,18 @@
 package com.intiformation.AppSchool.controller;
-import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	Logger log = Logger.getLogger(HomeController.class.getName());
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/Langue", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-	
-		return "index";
-	}
-
+	@GetMapping(value = "/index2")
+    public String welcomePage() {
+        log.info("INTO index");
+        return "index2";
+    }
 }//end class
